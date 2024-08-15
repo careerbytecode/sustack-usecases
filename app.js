5e13afee461d734418c24285a9300afa65bf9510
RajCloudX <32886886+rajcloudx@users.noreply.github.com>
2024-08-15 20:03:20 +0200

    Create app.js

---
+++ app.js
@@ -0,0 +1,4 @@
+const express = require("express");
+const app = express();
+app.get("*", (req, res) => res.send("<h1>Hello World!</h1>"));
+app.listen(8080, () => console.log("Listening on 8080"));
